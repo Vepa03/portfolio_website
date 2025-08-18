87b698bd5fa127b0acebbc13a5c21c2127b35442
Vepa Babayev <vepababayev03@gmail.com>
2025-08-18 22:50:27 +0200

    added route

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,15 @@ import { Hero } from "./components/Hero/Hero"
 import { Navbar } from "./components/Navbar/Navbar"
 import { Projects } from "./components/Projects/Projects"
 import { Videos } from "./components/Videos/Videos"
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className={styles.App}>
       <Navbar/>
+      <Routes>
+        <Route path="/experiences" element={<Experience/>}/>
+      </Routes>
       <Hero/>
       <About/>
       <Experience/>
--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,12 @@ import App from './App.jsx'
 import "@fontsource/outfit"
 import "@fontsource/roboto"
 
+import { BrowserRouter } from "react-router-dom";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
